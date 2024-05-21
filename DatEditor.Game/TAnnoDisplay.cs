901b3852d8e1d6375a1b8af83c45b50245b34ce8
Azuki <theorybreakerhacks@gmail.com>
2024-05-21 12:32:23 +0200

    Add TAnnoDisplay class (since it's correct in this case)

---
+++ DatEditor.Game/TAnnoDisplay.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatEditor.Game
+{
+    public class TAnnoDisplay : TAnno
+    {
+        public int DADSD;
+        public int dword63F8;
+
+        public List<IDisplayColors> m_SomeKindOfVector = new List<IDisplayColors>();
+        public int m_Count;
+
+        public int m_SomeCount;
+    }
+}
