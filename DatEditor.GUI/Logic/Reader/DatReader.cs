bcf18300ab044f9dcea3af815d6bd7861fe8213d
Azuki <im@super.cute>
2024-05-23 20:10:01 +0200

    Add ReadChunkSpecial and ReadChunkByLengthMask to DatReader

--- DatEditor.GUI/Logic/Reader/DatReader.cs
+++ DatEditor.GUI/Logic/Reader/DatReader.cs
@@ -44,6 +44,43 @@ namespace DatEditor.Logic.Reader
             return ReadBytes(canReadLength > 0xC && BaseStream.Position < BaseStream.Length ? 0xC : canReadLength);
         }
 
+        public byte[] ReadChunkSpecial()
+        {
+            var canReadLength = (int)(BaseStream.Length - BaseStream.Position);
+            if (canReadLength < 8)
+                return null;
+
+            var chunk = ReadBytes(8);
+            var v6 = BitConverter.ToUInt16(chunk, 6);
+            if (v6 >= 0xFF00)
+            {
+                var word = BitConverter.GetBytes(v6);
+                chunk[4] = word[0];
+                chunk[5] = word[1];
+
+                canReadLength = (int)(BaseStream.Length - BaseStream.Position);
+                if (canReadLength < 2)
+                    return null;
+                var newChunk = ReadBytes(2);
+                Array.Copy(newChunk, 0, chunk, 6, newChunk.Length);
+            }
+
+            return chunk;
+        }
+
+        public byte[] ReadChunkByLengthMask(byte[] maskChunk)
+        {
+            var secondLast = BitConverter.ToUInt16(maskChunk, 6);
+            var last = BitConverter.ToUInt16(maskChunk, 4);
+            var length = (last << 16) + secondLast;
+
+            var canReadLength = (int)(BaseStream.Length - BaseStream.Position);
+            if (canReadLength < length)
+                return null;
+
+            return ReadBytes(length);
+        }
+
         public byte[] ReadChunkNoTraverse()
         {
             var previousPosition = BaseStream.Position;
